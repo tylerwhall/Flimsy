375e8ad75b60a40195b01270029cb0b4bda2e42c
Tyler Hall <tylerwhall@gmail.com>
2009-11-25 17:42:58 -0500

    JSON works

--- Flimsy/flimsy/controllers/map.py
+++ Flimsy/flimsy/controllers/map.py
@@ -2,7 +2,21 @@ from tg import expose
 
 from flimsy.lib.base import BaseController
 
+from flimsy.model import DBSession, Sensor
+
 class MapController(BaseController):
     @expose('flimsy.templates.map')
     def index(self):
         return dict()
+
+    @expose('json')
+    def sensors(self):
+        sens = []
+        for row in DBSession.query(Sensor).all():
+            s = {}
+            s["name"] = row.name
+            s["lat"] = row.lat
+            s["lng"] = row.lng
+            s["flooded"] = row.flooded
+            sens.append(s)
+        return dict(sensors=sens)
